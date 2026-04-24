eb491db5b98bdcc59f4b911f8bf9d9f2d9be8118
Egor Syrov <egor-syrov@bk.ru>
2026-04-24 11:43:33 +0300

    feature: add client repo + service

---
+++ app/client/__init__.py
@@ -0,0 +1,5 @@
+from .client import CryptoAPIClient
+from .schemas import PriceDTO
+
+
+__all__ = ["CryptoAPIClient", "PriceDTO"]
---
+++ app/client/service.py
@@ -0,0 +1,21 @@
+from .client import CryptoAPIClient
+from .repository import PriceRepository
+
+
+class PriceSyncService:
+    def __init__(
+        self,
+        api_client: CryptoAPIClient,
+        repository: PriceRepository,
+    ):
+        self.api_client = api_client
+        self.repository = repository
+
+    async def sync(self, tickers: list[str]) -> dict:
+        try:
+            dtos = await self.api_client.fetch_prices(tickers)
+
+            saved_count = await self.repository.upsert_prices(dtos)
+
+        except Exception as e:
+            ...
